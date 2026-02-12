b84159574ce7be721a960e6fed3ec711dc16d41c
Ny Kanto RANDRIAMBOLOLONA <120046452+ny-kanto@users.noreply.github.com>
2026-02-12 12:06:12 +0300

    liste detail boutique user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list';
+// import { ProductListComponent } from './components/product-list/product-list';
 import { ShopListComponent } from './components/shop-list/shop-list';
-import { ProductDetailComponent } from './components/product-detail/product-detail';
-import { ProductDetailBoutiqueComponent } from './components/product-detail-boutique/product-detail-boutique';
+// import { ProductDetailComponent } from './components/product-detail/product-detail';
+// import { ProductDetailBoutiqueComponent } from './components/product-detail-boutique/product-detail-boutique';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login';
 import { HomeComponent } from './components/home/home';
+import { ShopManagementComponent } from './components/shop-management/shop-management';
+import { ShopDetailComponent } from './components/shop-detail/shop-detail';
+import { ShopDetailUserComponent } from './components/shop-detail-user/shop-detail-user';
 
 export const routes: Routes = [
   { path: 'boutiques', component: ShopListComponent },
-  { path: 'produits/boutique/:id', component: ProductListComponent },
-  { path: 'produit-boutique/:id', component:  ProductDetailBoutiqueComponent},
-  { path: 'produit/:id', component: ProductDetailComponent, },
+  { path: 'boutiques/:id', component: ShopDetailUserComponent },
+//   { path: 'produits/boutique/:id', component: ProductListComponent },
+//   { path: 'produit-boutique/:id', component:  ProductDetailBoutiqueComponent},
+//   { path: 'produit/:id', component: ProductDetailComponent, },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'admin/boutiques', component: ShopManagementComponent },
+  { path: 'admin/boutique-details/:id', component: ShopDetailComponent },
   { path: '', component: HomeComponent },
 ];
